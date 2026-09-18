5183c21d81890b4a95f942ac2facfafb9a9531d9
Cy-bit-commits <cyrusbahinting0@gmail.com>
2026-09-18 17:57:21 +0800

    user sign in, sign out,sign up and current user

---
+++ services/authService.ts
@@ -0,0 +1,72 @@
+import {supabase} from '@/lib/supabaseClient';
+
+
+// sign up a new user with email and password and
+//  store additional metadata in the profiles table
+export async function signUpUser({
+    email,
+    password,
+    fullName,
+    college,
+}:{
+    email: string;
+    password: string;
+    fullName: string;
+    college: string;
+}) {
+
+    // create a new user in supabase auth and
+    // store additional user metadata in the profiles table
+    
+    const {data, error} = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            data:{
+                full_name : fullName,
+                college : college || null,
+
+            },
+        },
+    });
+    //return the data and error from the sign up process
+    // if there is an error, log it and return null data and the error
+    if (error){
+        console.error('Sign up error:', error.message);
+        return {data: null, error};
+    }
+    return {data, error: null};
+
+}
+
+export async function signInUser(email: string, password: string){
+    const {data, error} = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    });
+
+    if (error){
+        console.error('Sign in error:', error.message);
+        return {data : null , error};
+    }
+    return {data, error: null};
+
+}
+
+export async function signOutUser(){
+    const {error} = await supabase.auth.signOut();
+    if (error){
+        console.error('Sign Out Error:', error.message);
+        return {error};
+    }
+    return {error: null};
+}
+
+export async function getCurrentUser(){
+    const {data: {user}, error} = await supabase.auth.getUser();
+    if (error){
+        console.error('Get Current User Error:', error.message);
+        return {user: null, error};
+    }
+    return {user, error: null};
+}
